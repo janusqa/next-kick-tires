194d8a246db072d22f2b0e82b4cc3727b79ddc93
Janus Developer <dev@retrievo.net>
2024-08-08 12:54:04 -0400

    set up prisma and first model, and perform a migration and update

--- app/_components/DrinksList.tsx
+++ app/_components/DrinksList.tsx
@@ -1,19 +1,29 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
 const DrinksList = ({
     drinks,
 }: {
-    drinks: { idDrink: string; strDrink: string }[];
+    drinks: { idDrink: string; strDrink: string; strDrinkThumb: string }[];
 }) => {
     return (
-        <ul className="menu menu-verticle pl-0">
+        <ul className="grid sm:grid-cols-2 gap-6 mt-6">
             {drinks.map((drink) => (
                 <li key={drink.idDrink}>
                     <Link
                         href={`/drinks/${drink.idDrink}`}
                         className="text-xl font-medium"
                     >
+                        <div className="relative h-48 mb-4">
+                            <Image
+                                src={drink.strDrinkThumb}
+                                fill
+                                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
+                                alt={drink.strDrink}
+                                className="rounded-md object-cover"
+                            />
+                        </div>
                         {drink.strDrink}
                     </Link>
                 </li>
--- app/drinks/[id]/page.tsx
+++ app/drinks/[id]/page.tsx
@@ -1,5 +1,7 @@
-import Link from 'next/link';
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import drinkImg from './drink.jpg';
 
 const DrinkDetailPage = async ({ params }: { params: { id: string } }) => {
     const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
@@ -15,17 +17,33 @@ const DrinkDetailPage = async ({ params }: { params: { id: string } }) => {
     };
 
     const data: {
-        drinks: { idDrink: string; strDrink: string; strThumb: string }[];
+        drinks: { idDrink: string; strDrink: string; strDrinkThumb: string }[];
     } = await getDrinkDetail(params.id);
 
     const title = data?.drinks[0].strDrink;
-    const imgSrc = data?.drinks[0].strThumb;
+    const imgSrc = data?.drinks[0].strDrinkThumb;
 
     return (
         <div>
             <Link href="/drinks" className="btn btn-primary mt-8 mb-12">
                 Back to drinks
             </Link>
+            {/** Remote Image -
+             * Need to set width/height.
+             * Need to also whitelist remote image base url in config */}
+            <Image
+                src={imgSrc}
+                width={300}
+                height={300}
+                className="w-48 h-48 rounded-lg shadow-lg mb-4 priority"
+                alt={title}
+            />
+            {/** Local Image */}
+            {/* <Image
+                src={drinkImg}
+                className="w-48 h-48 rounded-lg"
+                alt="A drink"
+            /> */}
             <h1 className="text-4xl mb-8">{title}</h1>
         </div>
     );
